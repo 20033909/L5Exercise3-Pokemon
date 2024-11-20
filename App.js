98fb6ce639139ff078fb3b5561c5d842d03b4a68
20033909 <20033909@myrp.edu.sg>
2024-11-20 21:49:29 +0800

    pokemon

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SectionList, Image, TouchableOpacity } from 'react-native';
+import { dataSource } from './components/datasource'; // Import dataSource from dataSource.js
+import styles from './components/styles';
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    // Render each Pokémon item
+    const renderItem = ({ item }) => (
+        <View style={styles.pokemon}>
+            {/* Render Pokémon name */}
+            <Text style={styles.pokemonName}>{item.key}</Text>
+            {/* Render the Pokémon image */}
+            <Image source={{ uri: item.link }} style={styles.pokemonImage} />
+        </View>
+    );
+
+    // Render the section header with a conditional background color
+    const renderSectionHeader = ({ section: { title } }) => {
+        let headerStyle = styles.sectionHeader; // Default section header style
+
+        // Set background color based on the title of the section
+        if (title === 'Fire Type') {
+            headerStyle = { ...headerStyle, ...styles.fireTypeHeader };
+        } else if (title === 'Lightning Type') {
+            headerStyle = { ...headerStyle, ...styles.lightningTypeHeader };
+        }
+
+        return (
+            <View style={headerStyle}>
+                <Text style={styles.sectionHeaderText}>{title}</Text>
+            </View>
+        );
+    };
+
+    return (
+        <View style={styles.container}>
+            <StatusBar style="auto" />
+
+            {/* Add Pokemon Button */}
+            <TouchableOpacity style={styles.button} onPress={() => {}}>
+                <View style={styles.buttonContent}>
+                    <Icon name="catching-pokemon" style={styles.iconSizeColor} />
+                    <Text style={styles.buttonText}>ADD POKEMON</Text>
+                    <Icon name="catching-pokemon" style={styles.iconSizeColor} />
+                </View>
+            </TouchableOpacity>
+
+            {/* SectionList to display Pokémon data by type */}
+            <SectionList
+                sections={dataSource}  // Use dataSource here
+                keyExtractor={(item, index) => item.key + index}  // Unique key for each item
+                renderItem={renderItem}  // Render each item using renderItem function
+                renderSectionHeader={renderSectionHeader}  // Custom header with conditional background color
+                contentContainerStyle={styles.scrollContainer}
+            />
+        </View>
+    );
+}
---
+++ components/datasource.js
@@ -0,0 +1,16 @@
+export const dataSource = [
+    {
+        title: 'Fire Type',
+        data: [
+            { key: 'Charmander', link: 'https://dz3we2x72f7ol.cloudfront.net/expansions/151/en-us/SV3pt5_EN_4.png' },
+            { key: 'Magmar', link: 'https://dz3we2x72f7ol.cloudfront.net/expansions/151/en-us/SV3pt5_EN_126.png' },
+        ]
+    },
+    {
+        title: 'Lightning Type',
+        data: [
+            { key: 'Raichu', link: 'https://dz3we2x72f7ol.cloudfront.net/expansions/151/en-us/SV3pt5_EN_26.png' },
+            { key: 'Jolteon', link: 'https://dz3we2x72f7ol.cloudfront.net/expansions/151/en-us/SV3pt5_EN_135.png' },
+        ]
+    }
+];
